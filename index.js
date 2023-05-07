d3d9bf9c9a97127ce286ade5202221e2a874a2f9
ariaan ahmed <you@example.com>
2023-05-07 20:35:52 +0600

    delete function implemented

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -42,6 +42,13 @@ async function run() {
         res.send(result)
     })
     
+    app.delete('/users/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log('please delete id from data base', id)
+        const query = { _id: new ObjectId(id) };
+        const result = await userCollection.deleteOne(query)
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
